a162c0e7710b0e77e41e5a702b359a5faa52b00b
Tailus Dev <4932abcd@gmail.com>
2024-02-24 03:34:14 +0330

    practice project got finished

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "./Input.js";
-import { useFetchWeather } from "../Hooks/useFetchWeather.js";
+import { useWeatherfetch } from "../Hooks/useWeatherfetch.js";
+import { Weather } from "./Weather.js";
+import { Loader } from "./Loader.js";
 
 const App = () => {
-  const [location, setLocation] = useState("");
-  const { weather, locationDisplay, errorMessage } = useFetchWeather(location);
+  const [location, setLocation] = useState(localStorage.getItem("location"));
+  const { weather, displayLoc, errorMessage, isLoading } = useWeatherfetch(location);
+
 
   return (
     <div className="app">
       <h1>Funcy Weather</h1>
       <Input location={location} onChange={setLocation} />
+      {isLoading && <Loader />}
+      {!isLoading && weather.time && (
+        <Weather
+          data={weather}
+          location={displayLoc}
+          error={errorMessage}
+          isLoading={isLoading}
+        />
+      )}
     </div>
   );
 };
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -2,5 +2,6 @@
 export const Input = ({location,onChange}) => {
     return (
         <input placeholder="search location..." className="input" value={location} onChange={(e) => onChange(e.target.value)}/>
-    )    
+        
+        )    
 }
